31cca51addf6aa491abbddb8e71d0bd065bb2618
myxu <1132254228@pfur.ru>
2025-10-26 22:22:22 +0300

    Lab 4:NASM assembly programs - hello.asm and lab4.asm
    EOF

---
+++ labs/lab04/lab4.asm
@@ -0,0 +1,18 @@
+; hello.asm
+SECTION .data
+hello:    DB 'Xu Mengyu', 10
+helloLen: EQU $-hello
+
+SECTION .text
+GLOBAL _start
+
+_start:
+    mov eax, 4
+    mov ebx, 1
+    mov ecx, hello
+    mov edx, helloLen
+    int 80h
+    mov eax, 1
+    mov ebx, 0
+    int 80h
+
